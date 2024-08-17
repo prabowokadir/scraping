f05ff26b275f6e7bb4c9b038af8b559897d66783
Muhammad Prabowo Kadir <prabowokadir@gmail.com>
2024-08-17 20:51:04 +0700

    Initialize to automate scraping other stats data

--- epl.py
+++ epl.py
@@ -7,10 +7,10 @@ from urllib.request import urlopen
 # open and read the HTML
 url = "https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats"
 
-response = requests.get(url)
-if response.status_code == 429:
-    retry_after = int(response.headers.get("Retry-After", 60)) # default wait for 60s if there're no header
-    time.sleep(retry_after)
+# response = requests.get(url)
+# if response.status_code == 429:
+#     retry_after = int(response.headers.get("Retry-After", 60)) # default wait for 60s if there're no header
+#     time.sleep(retry_after)
 
 page = urlopen(url)
 html = page.read().decode("utf-8")
@@ -27,39 +27,41 @@ links = [l.get("href") for l in links]
 links = [l for l in links if '/squads/' in l]
 
 team_urls = [f"https://fbref.com{l}" for l in links]
+team_urls = team_urls[0]
 
-for i in range(len(team_urls)):
-    team_name = team_urls[i].split("/")[-1].replace("-Stats", '').replace("-", " ")
+team_name = team_urls.split("/")[-1].replace("-Stats", '').replace("-", " ")
 
-    # open and read the HTML team
-    page_team = urlopen(team_urls[i])
-    html_team = page_team.read().decode("utf-8")
+# open and read the HTML team
+page_team = urlopen(team_urls)
+html_team = page_team.read().decode("utf-8")
 
-    # create BeautifulSoup object for HTML team
-    soup_team = BeautifulSoup(html_team, "html.parser")
+# create BeautifulSoup object for HTML team
+soup_team = BeautifulSoup(html_team, "html.parser")
 
-    # store the scores and fixtures data on pandas
-    matches = pd.read_html(html_team, match="Scores & Fixtures")
-    matches = matches[0]
+# store the scores and fixtures data on pandas
+matches = pd.read_html(html_team, match="Scores & Fixtures")
+matches = matches[0]
 
-    attr = ["shooting", "keeper", "passing", "passing_types", "gca", "defense", "possession", "misc"]
-    table = ["Shooting", "Goalkeeping", "Passing", "Pass Types", "Goal and Shot Creation",
-             "Defensive Actions", "Possession", "Miscellaneous Stats"]
+attr = ["shooting", "keeper", "passing", "passing_types", "gca", "defense", "possession", "misc"]
+table = ["Shooting", "Goalkeeping", "Passing", "Pass Types", "Goal and Shot Creation",
+            "Defensive Actions", "Possession", "Miscellaneous Stats"]
 
-    print(f"{team_name}:")
+print(f"{team_name}:")
 
-    for j in range(len(attr)):
-        data = soup_team.find_all("a")
-        data = [d.get("href") for d in data]
-        data = [d for d in data if d and f"all_comps/{attr[j]}/" in d]
-        data = f"https://fbref.com{data[0]}"
-        data = urlopen(data)
-        data = data.read().decode("utf-8")
-        data = pd.read_html(data, match=table[j])
-        data = data[0]
-        
-        print(f"Statistics of {table[j]} successfully retrieved")
-    
-    print("\n")
+# for j in range(len(attr)):
+data = soup_team.find_all("a")
+data = [d.get("href") for d in data]
+data = [d for d in data if d and f"all_comps/shooting/" in d]
+data = f"https://fbref.com{data[0]}"
+data = urlopen(data)
+data = data.read().decode("utf-8")
+data = pd.read_html(data, match="Shooting")
+data = data[0]
 
-    time.sleep(60)
+# print(f"Statistics of {table[j]} successfully retrieved")
+
+# print("\n")
+
+# time.sleep(60)
+
+print(data.head())
--- trial_epl.py
+++ trial_epl.py
@@ -4,19 +4,19 @@ import pandas_gbq
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-# open and read the HTML
-url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 
-# season
-years = list(range(2024, 2022, -1))
+start_year = 2023
+url = f"https://fbref.com/en/comps/9/{str(start_year)}-{str(start_year+1)}/{str(start_year)}-{str(start_year+1)}-Premier-League-Stats"
 
-for year in years:
+project_id = "sacred-bonbon-399703"
+
+for i in range(3):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
     table = soup.select("table.stats_table")[0]
 
-    links = [l.get("href") for l in table.find_all("a")]
+    links = [l.get("href") for l in table.find_all("a")] # find all the link to team
     links = [l for l in links if '/squads/' in l]
     team_urls = [f"https://fbref.com{l}" for l in links]
 
@@ -24,35 +24,57 @@ for year in years:
     url = f"https://fbref.com{previous_season}"
 
     for team_url in team_urls:
-        print(team_url)
-        print(url)
-        # team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+        team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
         
-        # # open and read the HTML team
-        # page_team = urlopen(team_url)
-        # html_team = page_team.read().decode("utf-8")
+        # open and read the HTML team
+        page_team = urlopen(team_url)
+        html_team = page_team.read().decode("utf-8")
 
-        # # create BeautifulSoup object for HTML team
-        # soup_team = BeautifulSoup(html_team, "html.parser")
+        # create BeautifulSoup object for HTML team
+        soup_team = BeautifulSoup(html_team, "html.parser")
 
-        # # store the scores and fixtures data on pandas
-        # matches = pd.read_html(html_team, match="Scores & Fixtures")
-        # matches = matches[0]
+        # store the scores and fixtures data on pandas
+        matches = pd.read_html(html_team, match="Scores & Fixtures")
+        matches = matches[0]
 
-        # matches.columns = [c.lower() for c in matches.columns]
-        # matches.columns = [c.replace(" ", "_") for c in matches.columns]
-        # matches["season"] = year
-        # matches["team_name"] = team_name
+        matches.columns = [c.lower() for c in matches.columns]
+        matches.columns = [c.replace(" ", "_") for c in matches.columns]
+        matches["season"] = start_year
+        matches["team_name"] = team_name
+        matches["gf"] = matches["gf"].astype("str")
+        matches["ga"] = matches["ga"].astype("str")
 
-        # pandas_gbq.to_gbq(
-        #     matches,
-        #     "raw.premier_league",
-        #     project_id="sacred-bonbon-399703",
-        #     if_exists="append"
-        # )
+        pandas_gbq.to_gbq(
+            matches,
+            "raw.premier_league_matches_stats",
+            project_id=project_id,
+            if_exists="append"
+        )
 
-        time.sleep(60)
+        attr = ["shooting", "keeper", "passing", "passing_types", "gca", "defense", "possession", "misc"]
+        table = ["Shooting", "Goalkeeping", "Passing", "Pass Types", "Goal and Shot Creation",
+                "Defensive Actions", "Possession", "Miscellaneous Stats"]
+        
+        for j in range(len(attr)):
+            data = soup_team.find_all("a")
+            data = [d.get("href") for d in data]
+            data = [d for d in data if d and f"all_comps/{attr[j]}/" in d]
+            data = f"https://fbref.com{data[0]}"
+            data = urlopen(data)
+            data = data.read().decode("utf-8")
+            data = pd.read_html(data, match=table[j])
+            data = data[0]
+            # need to add several line so the pandas column will not have multiple layer, create the lower case, replace " ", and changes data type
 
-    print(f"Premier League season {str(year-1)}-{str(year)} has been done.")
+            pandas_gbq.to_gbq(
+                data,
+                f"raw.premier_league_{attr[j]}_stats",
+                project_id=project_id,
+                if_exists="append"
+            )
 
-    time.sleep(60)
+    print(f"Premier League season {str(start_year)}-{str(start_year+1)} has been done.")
+
+    start_year-=1
+
+    time.sleep(5)
