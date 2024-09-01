64fb0cb5ef79a3393f9d0acb152296b75ce84bf3
Muhammad Prabowo Kadir <prabowokadir@gmail.com>
2024-09-01 20:28:36 +0700

    Update the main function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from credentials import get_credentials
 from scraper_matches import matches_data
-from scraper_shoots import shooting_data
+# from scraper_shoots import shooting_data
 from bigquery_utils import upload_to_bigquery
 import time
 import argparse
 
 def main(start_year, project_id, dataset_id, iterations):
     table_matches_id = f"{project_id}.{dataset_id}.premier_league_matches_stats"
-    table_shoots_id = f"{project_id}.{dataset_id}.premier_league_shoots_stats"
+    # table_shoots_id = f"{project_id}.{dataset_id}.premier_league_shoots_stats"
 
     # Get credentials
     credentials = get_credentials()
@@ -16,14 +16,14 @@ def main(start_year, project_id, dataset_id, iterations):
         # Scrape data
         url = f"https://fbref.com/en/comps/9/{start_year}-{start_year+1}/{start_year}-{start_year+1}-Premier-League-Stats"
         matches = matches_data(url, start_year)
-        shoots = shooting_data(url, start_year)
+        # shoots = shooting_data(url, start_year)
 
         # Store data to BigQuery
         upload_to_bigquery(matches, table_matches_id, project_id, credentials)
         
-        time.sleep(5)
+        # time.sleep(5)
 
-        upload_to_bigquery(shoots, table_shoots_id, project_id, credentials)
+        # upload_to_bigquery(shoots, table_shoots_id, project_id, credentials)
         print(f"Premier League season {start_year}-{start_year+1} data uploaded.")
 
         # Move to previous season
