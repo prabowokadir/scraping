4ec5ce289009dff2fc0397937ad22ff80feacd5f
Muhammad Prabowo Kadir <prabowokadir@Muhammads-MacBook-Air.local>
2024-09-17 20:46:02 +0700

    Modify the bookspider.py file

--- scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,11 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield{
+                'title' : book.css('h3 a').attrib['title'],
+                'price' : book.css('.product_price .price_color::text').get(),
+                'url' : book.css('h3 a').attrib['href']
+            }
