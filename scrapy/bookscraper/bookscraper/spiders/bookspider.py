3221647ba5a2f500fd66235f7e3404030833279f
Muhammad Prabowo Kadir <prabowokadir@gmail.com>
2024-09-19 20:05:26 +0700

    Update the bookspider.py

--- scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -15,3 +15,16 @@ class BookspiderSpider(scrapy.Spider):
                 'price' : book.css('.product_price .price_color::text').get(),
                 'url' : book.css('h3 a').attrib['href']
             }
+
+        next_page = response.css('li.next a::attr(href)').get() # to get the url to the next page
+
+        if next_page is not None: # check if there're more next page
+            if 'catalogue/' in next_page: # condition to check if the url is correct or not
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            
+            # to go to the next page url with 'follow'
+            # after that callback will execute after get the response from next page url
+            # which is will execute the parse function again
+            yield response.follow(next_page_url, callback=self.parse)
