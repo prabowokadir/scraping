852b08fd6af58825b2a4b84b4804510f3db5275e
Muhammad Prabowo Kadir <prabowokadir@gmail.com>
2024-09-19 20:30:56 +0700

    Open the book url

--- scrapy/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -10,21 +10,17 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield{
-                'title' : book.css('h3 a').attrib['title'],
-                'price' : book.css('.product_price .price_color::text').get(),
-                'url' : book.css('h3 a').attrib['href']
-            }
+            relative_url = book.css('h3 a::attr(href)').get() # to get the url of the book
 
-        next_page = response.css('li.next a::attr(href)').get() # to get the url to the next page
-
-        if next_page is not None: # check if there're more next page
-            if 'catalogue/' in next_page: # condition to check if the url is correct or not
-                next_page_url = 'https://books.toscrape.com/' + next_page
+            if 'catalogue/' in relative_url: # condition to check if the url is correct or not
+                book_url = 'https://books.toscrape.com/' + relative_url
             else:
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             
-            # to go to the next page url with 'follow'
-            # after that callback will execute after get the response from next page url
-            # which is will execute the parse function again
-            yield response.follow(next_page_url, callback=self.parse)
+            # to go to the book url with 'follow'
+            # after that callback will execute after get the response from book url
+            # which is will execute the parse_book_page function again
+            yield response.follow(book_url, callback=self.parse_book_page)
+
+    def parse_book_page(self, response):
+        pass
